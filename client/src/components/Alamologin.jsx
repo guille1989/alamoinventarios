eb3fd9a6f41328b453329fd980ad93851fa6620b
guille1989 <arboleda955@gmail.com>
2022-11-16 23:27:04 -0500

    Alamo app 11162022 v20

--- client/src/components/Alamologin.jsx
+++ client/src/components/Alamologin.jsx
@@ -33,7 +33,7 @@ class Alamologin extends Component {
             headers : {'Content-type':'application/json'},
             body: JSON.stringify({user: this.state.user, password: this.state.password})    
           }      
-          fetch('https://alamoinventario.com:443/api/autenticacionusuario', requestOptions)
+          fetch('https://44.201.244.11:443/api/autenticacionusuario', requestOptions)
               .then(response => response.json())
               .then(data => {
                     console.log(data)
